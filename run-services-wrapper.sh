bc1b03a9fe6741a568c36de360b77fe1bd6cbb21
Alexander Sergeychik <alexander.sergeychik@gmail.com>
2016-11-08 21:25:36 +0300

    hotfix

--- run-services-wrapper.sh
+++ run-services-wrapper.sh
@@ -4,13 +4,12 @@ if [ $TC_WAIT_FOR_PORT -gt 0 ]; then
 	
 	echo "Performing wait for a host/port"
 	
-	while [ ! -z `nc $TC_WAIT_FOR_HOST $TC_WAIT_FOR_PORT`]; do
+	while [ ! $(nc $TC_WAIT_FOR_HOST $TC_WAIT_FOR_PORT)]; do
 		echo "$(date): Waiting for $TC_WAIT_FOR_HOST:$TC_WAIT_FOR_PORT ...";
 		sleep 2;
 	done
 	
 fi
 
-echo "Starting services"
-
+echo "Starting services..."
 /run-services.sh
