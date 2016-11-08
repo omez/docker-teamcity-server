b132dbee19d2dec08fed4717bf7ea2ad44f15f66
Alexander Sergeychik <alexander.sergeychik@gmail.com>
2016-11-08 20:13:45 +0300

    Fix of NC

--- run-services-wrapper.sh
+++ run-services-wrapper.sh
@@ -4,8 +4,8 @@ if [ $TC_WAIT_FOR_PORT -gt 0 ]; then
 	
 	echo "Performing wait for a host/port"
 	
-	while ! echo exit | nc $TC_WAIT_FOR_HOST $TC_WAIT_FOR_PORT; do
-		echo "Waiting for $TC_WAIT_FOR_HOST:$TC_WAIT_FOR_PORT ...";
+	while [ ! -z `nc $TC_WAIT_FOR_HOST $TC_WAIT_FOR_PORT`]; do
+		echo "$(date): Waiting for $TC_WAIT_FOR_HOST:$TC_WAIT_FOR_PORT ...";
 		sleep 2;
 	done
 	
