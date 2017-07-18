cd5f7e9d3397271a06415a8831879bb3e5ae6152
Alexander Sergeychik <alexander@isolutions.by>
2017-07-18 21:35:28 +0300

    Added service data path copy

--- server/run-services-wrapper.sh
+++ server/run-services-wrapper.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# copy JDBC connector
+
+MYSQL_CONNECTOR_PATH="/mysql-connector-java-5.1.42/mysql-connector-java-5.1.42-bin.jar"
+MYSQL_CONNECTOR_TARGET_DIR="$TEAMCITY_DATA_PATH/lib/jdbc"
+mkdir -p $MYSQL_CONNECTOR_TARGET_DIR
+cp -u -t $MYSQL_CONNECTOR_TARGET_DIR $MYSQL_CONNECTOR_PATH
+
 if [ $TC_WAIT_FOR_PORT -gt 0 ]; then
 	
 	echo "Performing wait for a host/port"
